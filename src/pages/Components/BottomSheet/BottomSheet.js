280f376c82f475dddd1cc6bd7070f9ddaa549d33
Gowshik Prabhu <gowshikprabhu@gmail.com>
2021-07-13 17:49:22 +0530

    create url parameters ui

--- src/pages/Components/BottomSheet/BottomSheet.js
+++ src/pages/Components/BottomSheet/BottomSheet.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+// import CustomMultiInput from "../CustomMultiInput/CustomMultiInput";
 import DropdownInput from "../DropdownInput/DropdownInput";
 import TextInput from "../TextInput/TextInput";
 import "./BottomSheet.css";
+import { FaPlus, FaTimes } from "react-icons/fa";
+import Spacing from "../Spacing/Spacing";
 
 const BottomSheet = () => {
+  const [dualInputs, setDualInput] = useState([]);
   const handleChange = () => {};
+  const handleAdd = () => {
+    setDualInput((prevState) => [...prevState, { key: "", value: "" }]);
+  };
+  const handleRemove = (idx) => {
+    if (dualInputs.length > 0 && idx < dualInputs.length) {
+      let oldArray = dualInputs;
+      oldArray.splice(idx, 1);
+      setDualInput((prevState) => [...oldArray]);
+    }
+  };
+
   return (
     <div className="bottomsheet__container">
       <div className="bottomsheet__topbar">Hello</div>
@@ -20,36 +35,79 @@ const BottomSheet = () => {
           />
           <br />
           <br />
-          <DropdownInput
-            label={"Request"}
-            name={"method"}
-            options={[
-              { name: "GET", value: "GET" },
-              { name: "POST", value: "POST" }
-            ]}
-          />
-          <TextInput
-            name={"apiUrl"}
-            placeholder={"Request url"}
-            value={""}
-            onChange={handleChange}
+          <label className="textinput__label">Request</label>
+          <div className="bottomsheet__dualinput">
+            <DropdownInput
+              name={"method"}
+              options={[
+                { name: "GET", value: "GET" },
+                { name: "POST", value: "POST" }
+              ]}
+            />
+            <Spacing width={"10px"} height={"1px"} />
+            <TextInput
+              name={"apiUrl"}
+              placeholder={"Request url"}
+              value={""}
+              onChange={handleChange}
+              style={{ width: "100%" }}
+            />
+          </div>
+          <br />
+          <label className="textinput__label">URL Parameters</label>
+          <DualInput
+            handleChange={handleChange}
+            onAdd={handleAdd}
+            // onRemove={handleRemove}
           />
           <br />
-          <br />
-          <TextInput
-            label={"URL parameters"}
-            name={"urlparameters"}
-            placeholder={"key1=value1,key2=value2"}
-            value={""}
-            onChange={handleChange}
-            hint={"Enter key=value pairs seperated by comma"}
-          />
+          {dualInputs.length > 0
+            ? dualInputs.map((options, idx) => (
+                <>
+                  <DualInput
+                    key={idx}
+                    inputKey={options.key}
+                    inputValue={options.value}
+                    handleChange={handleChange}
+                    onAdd={handleAdd}
+                    onRemove={() => handleRemove(idx)}
+                  />
+                  <br />
+                </>
+              ))
+            : null}
         </div>
         <hr className="bottomsheet__verticalline" />
         <div className="bottomsheet__content__section2">Dubakur</div>
+        <hr className="bottomsheet__verticalline" />
+        <div className="bottomsheet__content__section3">Dubakur</div>
       </div>
     </div>
   );
 };
 
 export default BottomSheet;
+
+const DualInput = ({ handleChange, onAdd, onRemove }) => {
+  return (
+    <div className="bottomsheet__dualinput">
+      <TextInput
+        name={"urlparametersKey"}
+        placeholder={"Key"}
+        value={""}
+        onChange={handleChange}
+      />
+      <Spacing width={"10px"} height={"1px"} />
+      <TextInput
+        name={"urlparametersValue"}
+        placeholder={"Value"}
+        value={""}
+        onChange={handleChange}
+      />
+      <Spacing width={"10px"} height={"1px"} />
+      <FaPlus className="bottomsheet__icons" onClick={onAdd} />
+      <Spacing width={"10px"} height={"1px"} />
+      <FaTimes className="bottomsheet__icons" onClick={onRemove} />
+    </div>
+  );
+};
