182e1b9e83b21165546ba6ef2afaac97fdb584ce
Gowshik Prabhu <gowshikprabhu@gmail.com>
2021-07-13 14:53:30 +0530

    divide bottom sheet content and set styles

--- src/pages/Components/BottomSheet/BottomSheet.js
+++ src/pages/Components/BottomSheet/BottomSheet.js
@@ -7,39 +7,46 @@ const BottomSheet = () => {
   const handleChange = () => {};
   return (
     <div className="bottomsheet__container">
-      <div>
-        <DropdownInput
-          label={"Select resouce"}
-          name={"resouce"}
-          options={[
-            { name: "REST API", value: "REST" },
-            { name: "SOAP", value: "SOAP" }
-          ]}
-        />
-        <br />
-        <DropdownInput
-          label={"Method"}
-          name={"method"}
-          options={[
-            { name: "GET", value: "GET" },
-            { name: "POST", value: "POST" }
-          ]}
-        />
-        <TextInput
-          name={"apiUrl"}
-          placeholder={"Request url"}
-          value={""}
-          onChange={handleChange}
-        />
-        <br />
-        <TextInput
-          label={"URL parameters"}
-          name={"urlparameters"}
-          placeholder={"key1=value1,key2=value2"}
-          value={""}
-          onChange={handleChange}
-          hint={"Enter key=value pairs seperated by comma"}
-        />
+      <div className="bottomsheet__topbar">Hello</div>
+      <div className="bottomsheet__content">
+        <div className="bottomsheet__content__section1">
+          <DropdownInput
+            label={"Select resource"}
+            name={"resource"}
+            options={[
+              { name: "REST API", value: "REST" },
+              { name: "SOAP", value: "SOAP" }
+            ]}
+          />
+          <br />
+          <br />
+          <DropdownInput
+            label={"Request"}
+            name={"method"}
+            options={[
+              { name: "GET", value: "GET" },
+              { name: "POST", value: "POST" }
+            ]}
+          />
+          <TextInput
+            name={"apiUrl"}
+            placeholder={"Request url"}
+            value={""}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <TextInput
+            label={"URL parameters"}
+            name={"urlparameters"}
+            placeholder={"key1=value1,key2=value2"}
+            value={""}
+            onChange={handleChange}
+            hint={"Enter key=value pairs seperated by comma"}
+          />
+        </div>
+        <hr className="bottomsheet__verticalline" />
+        <div className="bottomsheet__content__section2">Dubakur</div>
       </div>
     </div>
   );
