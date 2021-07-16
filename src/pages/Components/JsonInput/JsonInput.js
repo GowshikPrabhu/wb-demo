17b1c6bb8bc8721ca7eb2eb381d2bd2f62b7b1af
Gowshik Prabhu <gowshikprabhu@gmail.com>
2021-07-16 20:32:31 +0530

    JsonInput component create

--- src/pages/Components/JsonInput/JsonInput.js
+++ src/pages/Components/JsonInput/JsonInput.js
@@ -2,8 +2,42 @@ import React from "react";
 import "./JsonInput.css";
 import ReactJson from "react-json-view";
 
-const JsonInput = ({ value }) => {
-  return <ReactJson defaultValue={null} src={value} />;
+const JsonInput = ({ value, name = "root", onChange }) => {
+  const onAdd = (e) => {
+    if (e.new_value === "error") {
+      return false;
+    }
+    console.log(e);
+    onChange(e.updated_src);
+  };
+  const onDelete = (e) => {
+    if (e.new_value === "error") {
+      return false;
+    }
+    console.log(e);
+    onChange(e.updated_src);
+  };
+  const onEdit = (e) => {
+    if (e.new_value === "error") {
+      return false;
+    }
+    console.log(e);
+    onChange(e.updated_src);
+  };
+  return (
+    <ReactJson
+      defaultValue={null}
+      src={value}
+      name={name}
+      iconStyle={"triangle"}
+      enableClipboard={true}
+      displayDataTypes={true}
+      displayObjectSize={true}
+      onDelete={onDelete}
+      onAdd={onAdd}
+      onEdit={onEdit}
+    />
+  );
 };
 
 export default JsonInput;
