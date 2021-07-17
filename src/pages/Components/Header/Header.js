68dd2273bee4a51cc1d425a0e57f18ba1975c0d2
Gowshik Prabhu <gowshikprabhu@gmail.com>
2021-07-18 02:50:56 +0530

    create functionality props

--- src/pages/Components/Header/Header.js
+++ src/pages/Components/Header/Header.js
@@ -3,19 +3,33 @@ import "./Header.css";
 import { AiOutlineMenu } from "react-icons/ai";
 import { FaPlay } from "react-icons/fa";
 
-const Header = () => {
+const Header = ({
+  title = "Project Name",
+  onToggleSidebar,
+  isSaved,
+  onShare,
+  onPublish,
+  onView,
+  onClickAccount
+}) => {
   return (
     <header>
       <div className="header__left">
-        <AiOutlineMenu className="header__menuIcon" />
-        <h4 className="header__title">Project Name</h4>
-        <span className="header__saved">(Saved)</span>
+        <AiOutlineMenu className="header__menuIcon" onClick={onToggleSidebar} />
+        <h4 className="header__title">{title}</h4>
+        <span className="header__saved">
+          {isSaved ? "(Saved)" : "(Unsaved)"}
+        </span>
       </div>
       <div className="header__right">
-        <FaPlay className="header__playIcon" />
-        <button className="header__shareButton">Share</button>
-        <button className="header__publishButton">Publish</button>
-        <button className="header__accountButton">
+        <FaPlay className="header__playIcon" onClick={onView} />
+        <button className="header__shareButton" onClick={onShare}>
+          Share
+        </button>
+        <button className="header__publishButton" onClick={onPublish}>
+          Publish
+        </button>
+        <button className="header__accountButton" onClick={onClickAccount}>
           <img src="https://via.placeholder.com/150" alt="Account" />
         </button>
       </div>
