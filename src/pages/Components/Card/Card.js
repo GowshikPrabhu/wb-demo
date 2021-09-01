5ae574d3956a2cf3fd8ed640a9103fdde49d7922
Gowshik Prabhu <gowshikprabhu@gmail.com>
2021-09-01 23:18:17 +0530

    create simple card

---
+++ src/pages/Components/Card/Card.js
@@ -0,0 +1,42 @@
+import React, { useContext, useEffect, useState } from "react";
+import Draggable from "react-draggable";
+import { Context } from "../../../context/Context";
+import DataTable from "react-data-table-component";
+
+const Card = ({ customStyles, content, id }) => {
+  const [state] = useContext(Context);
+
+  const renderComponent = (component, index) => {
+    var newComponent;
+    if (component.type === "table") {
+      console.log(component.columns);
+      newComponent = (
+        <DataTable title="" columns={component.columns} data={component.data} />
+      );
+    }
+    return newComponent;
+  };
+
+  return (
+    <div
+      style={{
+        ...customStyles.card,
+        border:
+          state.selectedComponentIndex !== -1
+            ? state.componentsList[state.selectedComponentIndex].id === id
+              ? "2px solid #cecece"
+              : customStyles.card.border
+            : customStyles.card.border
+      }}
+    >
+      <div style={customStyles.cardHeader}>{content.title}</div>
+      <div style={customStyles.cardContent}>
+        {content.components.map((component, index) => {
+          return renderComponent(component, index);
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
